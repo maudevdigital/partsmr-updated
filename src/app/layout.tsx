46b87feb93836d53ffb6c5406f4ae0da8a421af7
maudevcl <162978270+maudevcl@users.noreply.github.com>
2025-10-26 22:29:39 -0300

    cambios gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,24 +56,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MGW5KZ7');`}
+})(window,document,'script','dataLayer','GTM-MGW5KZZ7');`}
         </Script>
         {/* End Google Tag Manager */}
-
-        {/* Google tag (gtag.js) - Conversiones */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16953811243"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16953811243');
-          `}
-        </Script>
       </head>
       <body
         className={`${montserrat.variable} font-sans antialiased bg-[#f9fafb] text-[#0f172a] overflow-x-hidden`}
@@ -81,7 +66,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MGW5KZ7"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MGW5KZZ7"
             height="0" 
             width="0" 
             style={{display:'none',visibility:'hidden'}}
